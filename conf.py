30b8156d4bc0fc60275e918be329581e4af53d58
dalaocu <njumjy06@126.com>
2021-02-02 23:53:51 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -332,8 +332,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, '
-     Blockchain Gov Doc', u'WeBankBlockchain Gov Documentation',
+    (master_doc, 'Blockchain Gov Doc', u'WeBankBlockchain Gov Documentation',
      [author], 1)
 ]
 
